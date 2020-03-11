3ff36f25475cd590c452ea20c7367cf16628d2a3
Dannier Li <sherryandchloe@gmail.com>
2020-03-11 00:29:59 -0700

    Add 'get_psnr' method to 'vcompresslib' module
    Add `get_psnr` method to 'vcompresslib' module.
    The method takes in all the test images and decoded images and calculate the average PSNR.

--- task3/vcompresslib.py
+++ task3/vcompresslib.py
@@ -1,5 +1,8 @@
+import numpy as np
+import tensorflow as tf
+
 from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Input, Dense, Conv2D, Conv2DTranspose, MaxPooling2D, UpSampling2D
 
 def test_model(train_data, patch_shape):
     input_img = Input(shape=patch_shape)
@@ -7,7 +10,7 @@ def test_model(train_data, patch_shape):
     e = Conv2D(64, (7, 7), activation='relu', padding='same')(input_img)
     e = MaxPooling2D((2, 2), padding='same')(e)
     e = Conv2D(32, (5, 5), activation='relu', padding='same')(e)
-    #e = MaxPooling2D((2, 2), padding='same')(e)
+    e = MaxPooling2D((2, 2), padding='same')(e)
     e = Conv2D(16, (1, 1), activation='relu', padding='same')(e)
     e = Conv2D(8, (3, 3), activation='relu', padding='same')(e)
     encoded = Conv2D(4, (3, 3), activation='relu', padding='same')(e)
@@ -15,7 +18,7 @@ def test_model(train_data, patch_shape):
     d = Conv2D(4, (3, 3), activation='relu', padding='same')(encoded)
     d = Conv2D(8, (3, 3), activation='relu', padding='same')(d)
     d = Conv2D(16, (1, 1), activation='relu', padding='same')(d)
-    #d = UpSampling2D((2, 2))(d)
+    d = UpSampling2D((2, 2))(d)
     d = Conv2D(32, (5, 5), activation='relu', padding='same')(d)
     d = UpSampling2D((2, 2))(d)
     d = Conv2D(64, (7, 7), activation='relu', padding='same')(d)
@@ -28,3 +31,17 @@ def test_model(train_data, patch_shape):
     autoencoder.fit(train_data, train_data, epochs=1, batch_size=25)
 
     return autoencoder
+
+def get_psnr(test_image, decoded_image):
+
+    PSNR = 0
+    n_test = test_image.shape[0]
+
+    for i in range(n_test):
+        MSE = tf.keras.losses.MeanSquaredError()
+        test_mse = MSE(test_image[i], decoded_image[i])
+        PSNR += 10.0 * np.log10(1.0 / test_mse)
+    
+    PSNR /= n_test
+
+    return PSNR
