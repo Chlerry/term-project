2489b2ef3af63f2088dce363fbfbca31c4e2decf
Dannier Li <sherryandchloe@gmail.com>
2020-03-14 00:27:11 -0700

    Move Model 1 to `vcompresslib`
    Move Model 1 to `vcompresslib`

--- task3/vcompresslib.py
+++ task3/vcompresslib.py
@@ -7,6 +7,57 @@ from keras.layers import Input, Dense, Conv2D, Conv2DTranspose, MaxPooling2D, Up
 import imgpatch
 
 # Ratio options are: 1/32, 1/16, 1/8, 1/4, 1/2
+def model1(train_data, patch_shape, ratio):
+    input_img = Input(shape=patch_shape)
+
+    e = Conv2D(64, (7, 7), activation='relu', padding='same')(input_img)
+    if(ratio == '1/2'):
+        e = MaxPooling2D((2, 1), padding='same')(e)
+    else:
+        e = MaxPooling2D((2, 2), padding='same')(e)
+
+    e = Conv2D(32, (5, 5), activation='relu', padding='same')(e)
+    if(ratio == '1/8'):
+        e = MaxPooling2D((2, 1), padding='same')(e)
+    elif (ratio == '1/16' or ratio == '1/32'):
+        e = MaxPooling2D((2, 2), padding='same')(e)
+
+    e = Conv2D(16, (1, 1), activation='relu', padding='same')(e)
+    if (ratio == '1/32'):
+        e = MaxPooling2D((2, 1), padding='same')(e)
+        
+    e = Conv2D(8, (3, 3), activation='relu', padding='same')(e)
+    encoded = Conv2D(3, (3, 3), activation='relu', padding='same')(e)
+
+    d = Conv2D(3, (3, 3), activation='relu', padding='same')(encoded)
+    d = Conv2D(8, (3, 3), activation='relu', padding='same')(d)
+    
+    if (ratio == '1/32'):
+        d = UpSampling2D((2, 1))(d)
+    d = Conv2D(16, (1, 1), activation='relu', padding='same')(d)
+    
+    if(ratio == '1/8'):
+        d = UpSampling2D((2, 1))(d)
+    elif (ratio == '1/16' or ratio == '1/32'):
+        d = UpSampling2D((2, 2))(d)
+    d = Conv2D(32, (5, 5), activation='relu', padding='same')(d)
+    
+    if(ratio == '1/2'):
+        d = UpSampling2D((2, 1))(d)
+    else:
+        d = UpSampling2D((2, 2))(d)
+    d = Conv2D(64, (7, 7), activation='relu', padding='same')(d)
+    decoded = Conv2D(3, (1, 1), activation='linear')(d)
+
+    autoencoder = Model(input_img, decoded)
+    autoencoder.summary()
+    autoencoder.compile(optimizer='adam', loss='mse')
+
+    autoencoder.fit(train_data, train_data, epochs=1, batch_size=25)
+
+    return autoencoder
+
+# Ratio options are: 1/32, 1/16, 1/8, 1/4, 1/2
 def model2(train_data, patch_shape, ratio):
     input_img = Input(shape=patch_shape)
 
